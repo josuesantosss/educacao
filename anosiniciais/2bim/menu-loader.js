16fb6113f50671c65d75c859ff010ca252a5a0a1
JOSUÉ <josue_santos@icloud.com>
2025-03-13 16:09:22 -0300

    Update menu-loader.js

--- anosiniciais/2bim/menu-loader.js
+++ anosiniciais/2bim/menu-loader.js
@@ -1,4 +1,4 @@
-fetch('../anosiniciais/1bim/menu.html')
+fetch('https://github.com/josuesantosss/educacao/blob/ff0be37cc786bcae25321eb864cbc3b039ed1316/anosiniciais/menu.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('menu-container').innerHTML = data;
