3acfacebc307a3c81c52970da1de3ade143a6a6e
JOSUÉ <josue_santos@icloud.com>
2025-03-26 22:44:15 -0300

    Update creditos-loader.js

--- creditos-loader.js
+++ creditos-loader.js
@@ -2,19 +2,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     const creditosHTML = `
         <div class="creditos">
-            Site criado por <a href="https://www.instagram.com/josuesantos" target="_blank">Josué Santos</a>
+            <span class="texto-branco">Site criado por </span>
+            <a href="https://www.instagram.com/josuesantos" target="_blank">Josué Santos</a>
         </div>
     `;
     
-    // Cria um container para os créditos
     const container = document.createElement('div');
     container.id = 'creditos-container';
     container.innerHTML = creditosHTML;
-    
-    // Insere no final do body
     document.body.appendChild(container);
     
-    // Adiciona o CSS dinamicamente
     const style = document.createElement('style');
     style.textContent = `
         .creditos {
@@ -23,6 +20,8 @@ document.addEventListener('DOMContentLoaded', function() {
             background-color: #000000;
             font-family: 'Google Sans', Roboto, sans-serif;
             font-size: 0.666em;
+        }
+        .creditos .texto-branco {
             color: white;
         }
         .creditos a {
