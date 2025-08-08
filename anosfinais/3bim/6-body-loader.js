40880e1873e5fa3f5f4ec7ce92fda00de93b7c88
JOSUÉ <josue_santos@icloud.com>
2025-08-08 15:36:21 -0300

    Update 6-body-loader.js

--- anosfinais/3bim/6-body-loader.js
+++ anosfinais/3bim/6-body-loader.js
@@ -3,3 +3,10 @@ fetch('6-body.html')
     .then(data => {
         document.getElementById('6-body-container').innerHTML = data;
     });
+
+fetch('7-body.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('7-body-container').innerHTML = data;
+    });
+
