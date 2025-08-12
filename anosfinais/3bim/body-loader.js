2275a17ee2e478e247efbdc53f96d8ac74ba61d5
JOSUÉ <josue_santos@icloud.com>
2025-08-12 15:02:56 -0300

    Update body-loader.js

--- anosfinais/3bim/body-loader.js
+++ anosfinais/3bim/body-loader.js
@@ -1,20 +1,26 @@
-const pages = [6, 7, 8, 9];
+// Pega o nome do arquivo atual (ex: "pagina6.html")
+const currentPage = window.location.pathname.split("/").pop();
 
-pages.forEach(num => {
-    fetch(`${num}-body.html`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Erro ao carregar ${num}-body.html`);
-            }
-            return response.text();
-        })
-        .then(data => {
-            const container = document.getElementById(`${num}-body-container`);
-            if (container) {
-                container.innerHTML = data;
-            } else {
-                console.warn(`Elemento #${num}-body-container não encontrado.`);
-            }
-        })
-        .catch(error => console.error(error));
-});
+// Mapeia o nome da página para o conteúdo que deve ser carregado
+const pageMap = {
+    "6ano.html": ["6"],
+    "7ano.html": ["7"],
+    "8abo.html": ["8"],
+    "9ano.html": ["9"]
+};
+
+// Verifica se a página atual está no mapeamento
+if (pageMap[currentPage]) {
+    pageMap[currentPage].forEach(num => {
+        fetch(`${num}-body.html`)
+            .then(response => {
+                if (!response.ok) throw new Error(`Erro ao carregar ${num}-body.html`);
+                return response.text();
+            })
+            .then(data => {
+                const container = document.getElementById(`${num}-body-container`);
+                if (container) container.innerHTML = data;
+            })
+            .catch(error => console.error(error));
+    });
+}
